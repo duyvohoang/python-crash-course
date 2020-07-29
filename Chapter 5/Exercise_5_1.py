daf81eb5acf43311b668f94bd15239b0fb297606
Duy Vo <hoangduyvo@gmail.com>
2020-07-28 23:45:09 -0500

    exercise 5.1

---
+++ Chapter 5/Exercise_5_1.py
@@ -0,0 +1,8 @@
+# The exercise 5.1 of the book "Python Crash Course"
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'toyota'? I predict False.")
+print(car == 'toyota')
