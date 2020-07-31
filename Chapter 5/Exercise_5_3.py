8d5bfcd72ba6ff8db114a8c246411f6f0907c51d
Duy Vo <hoangduyvo@gmail.com>
2020-07-30 22:34:11 -0500

    finish exercise 5.3

---
+++ Chapter 5/Exercise_5_3.py
@@ -0,0 +1,11 @@
+# Exercise 5.3 of the book "Python Crash Course"
+
+# alien_color = 'red'
+#
+# if alien_color == 'green':
+#	print("You just got 5 points!")
+
+alien_color = 'green'
+
+if alien_color == 'green':
+	print("You just got 5 points!")
