b4dfe8fab040e7159310a56759949b332504ba88
Duy Vo <hoangduyvo@gmail.com>
2020-07-30 23:50:36 -0500

    finish exercise 5.7

---
+++ Chapter 5/Exercise_5_7.py
@@ -0,0 +1,13 @@
+# Exercise 5.7 of the book "Python Crash Course"
+
+favorite_fruits = ['watermelon', 'banana', 'orange']
+if 'grape' in favorite_fruits:
+	print("You really like grapes!")
+if 'cherry' in favorite_fruits:
+	print("You really like cherries!")
+if 'orange' in favorite_fruits:
+	print("You really like oranges!")
+if 'banana' in favorite_fruits:
+	print("You really like bananas!")
+if 'watermelon' in favorite_fruits:
+	print("You really like watermelons!")
