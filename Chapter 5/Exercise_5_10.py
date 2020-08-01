89af0a312a50fef69a3728ee903234599284adec
Duy Vo <hoangduyvo@gmail.com>
2020-07-31 22:21:29 -0500

    finish 5.10 and 5.11

--- Chapter 5/Exercise_5_10.py
+++ Chapter 5/Exercise_5_10.py
@@ -7,6 +7,6 @@ current_users_lower = [user.lower() for user in current_users]
 
 for new_user in new_users:
 	if new_user.lower() in current_users:
-		print("You need to enter a new username.")
+		print(f"You need to enter a new username, {new_user} is not available.")
 	else:
 		print(f"\n{new_user} is available.")
