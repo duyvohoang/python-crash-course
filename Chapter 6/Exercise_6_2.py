2d968677702a862c7b63f4335cbd619731915bc6
Duy Vo <hoangduyvo@gmail.com>
2020-08-01 20:44:13 -0500

    finish the exercise 6-2

---
+++ Chapter 6/Exercise_6_2.py
@@ -0,0 +1,14 @@
+# Exercise 6-2 of the book "Python Crash Course"
+
+favorite_numbers = {
+	'david': 9,
+	'john': 5,
+	'jack': 6,
+	'wylie': 3,
+	'phill': 1,
+	}
+print(f"David: {favorite_numbers['david']}")
+print(f"John: {favorite_numbers['john']}")
+print(f"Jack: {favorite_numbers['jack']}")
+print(f"Wylie: {favorite_numbers['wylie']}")
+print(f"Phill: {favorite_numbers['phill']}")
