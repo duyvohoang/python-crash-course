bd623119778385da4f950b63bf1f87af20aab21d
Duy Vo <hoangduyvo@gmail.com>
2020-08-01 20:09:25 -0500

    finish the exercise 6-1

---
+++ Chapter 6/Exercise_6_1.py
@@ -0,0 +1,7 @@
+# Exercise 6-1 of the book "Python Crash Course"
+
+person = {'first_name': 'david', 'last_name': 'smith', 'age': 45, 'city': 'new york',}
+print(f"{person['first_name'].title()}")
+print(f"{person['last_name'].title()}")
+print(f"{person['age']}")
+print(f"{person['city'].title()}")
